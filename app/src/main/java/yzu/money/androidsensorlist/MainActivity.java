4ff957a5d1cf7d7e48be71a11d0702544bda2392
howdyhi <money@AnonymousdeMacBook-Air.local>
2017-01-18 13:17:49 +0800

    Show Android Sensor List

--- app/src/main/java/yzu/money/androidsensorlist/MainActivity.java
+++ app/src/main/java/yzu/money/androidsensorlist/MainActivity.java
@@ -1,13 +1,33 @@
 package yzu.money.androidsensorlist;
 
-import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
-
+class AndroidSensorList extends ListActivity {
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        SensorManager sensorManager
+                = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> listSensor
+                = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        List<String> listSensorType = new ArrayList<String>();
+        for(int i=0; i<listSensor.size(); i++){
+            listSensorType.add(listSensor.get(i).getName());
+        }
+
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listSensorType));
+        getListView().setTextFilterEnabled(true);
     }
 }
